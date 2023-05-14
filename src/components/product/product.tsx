cc685ccf4fcf6d951c77b12155d637ccbdb79145
ShlykovAA <anatolio7z@gmail.com>
2023-05-15 02:06:06 +0300

    add sass

--- src/components/product/product.tsx
+++ src/components/product/product.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import "./product.css"
+import styles from "./product.module.scss"
 
 export interface IProduct {
     src: string;
@@ -12,16 +12,22 @@ export interface IProduct {
 export const Product: FC<IProduct> = ({src, title, description, price, revers}) => {
     let element:string;
     if (revers) {
-        element = "card_revers"
+        element = styles.card_revers;
     } else {
         element = "";
     }
-    return <div className={`bread_card ${element}`}>
-        <img src={src} className="bread_img"/>
-        <div className="bread_info">
-            <h4 className="bread_title">{title}</h4>
-            <p>{description}</p>
-            <h5 className="bread_price">$ {price}</h5>
+    return (
+        <div className={`${styles.bread_card} ${element}`}>
+            <div className={styles.control}>
+                <img src={src} className={styles.bread_img}/>
+            </div>
+            <div className={styles.control}>
+                <div className={styles.bread_info}>
+                    <h4 className={styles.bread_title}>{title}</h4>
+                    <p>{description}</p>
+                    <h5 className={styles.bread_price}>$ {price}</h5>
+                </div>
+            </div>
         </div>
-    </div>;
+    );
 }
--- src/sections/assortment/assortment.tsx
+++ src/sections/assortment/assortment.tsx
@@ -1,27 +1,30 @@
 import { Product, IProduct } from "../../components/product";
-import reactLogo from '../../assets/react.svg'
-import viteLogo from '/vite.svg'
-import "./assortment.css"
+import styles from "./assortment.module.scss"
+import sourdoughLoaf from "/1.jpg";
+import baguette from "/rr.jpg";
+import farmersLoaf from "/gh.jpg";
+import roundBread from "/trrt.jpg"
+
 
 const config: IProduct[] = [
     {
-        src: viteLogo,
+        src: sourdoughLoaf,
         title: "Sourdough Loaf",
         description: "Sample text, consectetur adipiscing.",
         price: 4.65,
     }, {
-        src: reactLogo,
+        src: baguette,
         title: "Baguette",
         description: "Small-batch sourdough baguette.",
         price: 3.05,
     }, {
-        src: viteLogo,
+        src: farmersLoaf,
         title: "Farmers Loaf",
         description: "Slow-fermented sourdough Rye studded with Kalamata.",
         price: 5.50,
         revers: true,
     }, {
-        src: reactLogo,
+        src: roundBread,
         title: "Round Bread",
         description: "Made with 100% whole grain flour and high in fiber.",
         price: 5.15,
@@ -30,7 +33,7 @@ const config: IProduct[] = [
 
 export const Assortment = () => {
     return (
-        <div className="main_bread_div">
+        <div className={styles.main_bread_div}>
             {config.map((item)=> (<Product {...item}/>))}
         </div>
     )
