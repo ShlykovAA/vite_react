3790a2c72156be3559bd4632dca10203e0381af5
ShlykovAA <anatolio7z@gmail.com>
2023-06-07 15:58:01 +0300

    useForm with Yup

--- src/sections/form/form.tsx
+++ src/sections/form/form.tsx
@@ -1,40 +1,55 @@
-import { useForm } from "react-hook-form"
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from "yup";
 
 interface IForm {
-    name: string;
-    email: string;
+    firstName: string;
+    lastName: string;
 }
 
+const validationSchema = Yup.object({
+    firstName: Yup.string().required("Required"),
+    lastName: Yup.string().required("Required"),
+})
+
 export const Form = () => {
-    const { formState, getValues, handleSubmit, register, watch } = useForm<IForm>({
-        mode: "onSubmit",
-        reValidateMode: "onBlur",
-        defaultValues: {name: "", email: ""},
-        criteriaMode: "firstError",
-        shouldFocusError: true,
+    const [submitted, setSubmitted] = React.useState<boolean>(false);
+    const { handleSubmit, register, formState, getValues } = useForm<IForm>({
+        mode: "onBlur",
+        resolver: yupResolver(validationSchema) 
     });
 
-    console.log("formState", formState)
-    console.log("watch", watch("name"))
-    console.log("watch[]", watch(['name', 'email']))
-    console.log("getValues", getValues())
-
-    const onSubmit = (values: any, event: any) => {
-        console.log("onSubmit", values)
-        console.log("onSubmit 2", event)
+    const onSubmit = (values: any) => {
+        setSubmitted(true);
     }
+    const values = getValues();
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <label>
-                Name:
-                <input type="text" {...register('name')} />
-            </label>
-            <label>
-                Email:
-                <input type="text" {...register('email')} />
-            </label>
-            <button type="submit">Submit</button>
-        </form>
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>
+                    First Name:
+                    <input type="text" {...register('firstName')} />
+                </label>
+                { formState.errors.firstName && formState.touchedFields.firstName && (
+                    <p>{formState.errors.firstName.message}</p>
+                )}
+                <br />
+                <label>
+                    Last Name:
+                    <input type="text" {...register('lastName')} />
+                </label>
+                { formState.errors.lastName && formState.touchedFields.lastName && (
+                    <p>{formState.errors.lastName.message}</p>
+                )}
+                <br />
+                <button type="submit">Submit</button>
+            </form>
+            <div>
+                <p>First name: {values.firstName}</p>
+                <p>Last name: {values.lastName}</p>
+            </div>
+        </>
     )
 }
