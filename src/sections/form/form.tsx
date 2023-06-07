f47caeaad168ed547a5ef08c03dd6538fc162869
ShlykovAA <anatolio7z@gmail.com>
2023-06-07 18:53:31 +0300

    lesson 42 end

--- src/sections/form/form.tsx
+++ src/sections/form/form.tsx
@@ -1,21 +1,27 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from "yup";
 
 interface IForm {
-    firstName: string;
-    lastName: string;
+    email: string;
+    userName: string;
+    password: string;
 }
 
 const validationSchema = Yup.object({
-    firstName: Yup.string().required("Required"),
-    lastName: Yup.string().required("Required"),
+    email: Yup.string().required("Required").email("Incorrect email"),
+    userName: Yup.string().required("Required"),
+    password: Yup.string()
+        .required("Required")
+        .min(8,"Password is too short")
+        .matches(/\d+/,"Password should have at least one number")
+        .matches(/[A-Z]+/,"Password should have at least one uppercase letter"),
 })
 
 export const Form = () => {
     const [submitted, setSubmitted] = React.useState<boolean>(false);
-    const { handleSubmit, register, formState, getValues } = useForm<IForm>({
+    const { handleSubmit, register, formState, getValues, control } = useForm<IForm>({
         mode: "onBlur",
         resolver: yupResolver(validationSchema) 
     });
@@ -29,26 +35,37 @@ export const Form = () => {
         <>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <label>
-                    First Name:
-                    <input type="text" {...register('firstName')} />
+                    Email:
+                    <input type="text" {...register('email')} />
                 </label>
-                { formState.errors.firstName && formState.touchedFields.firstName && (
-                    <p>{formState.errors.firstName.message}</p>
+                { formState.errors.email && formState.touchedFields.email && (
+                    <p>{formState.errors.email.message}</p>
                 )}
                 <br />
                 <label>
-                    Last Name:
-                    <input type="text" {...register('lastName')} />
+                    User Name:
+                    <Controller name="userName" defaultValue="" control={control} render={
+                        ({field}) => <input {...field} />
+                    } />
                 </label>
-                { formState.errors.lastName && formState.touchedFields.lastName && (
-                    <p>{formState.errors.lastName.message}</p>
+                { formState.errors.userName && formState.touchedFields.userName && (
+                    <p>{formState.errors.userName.message}</p>
+                )}
+                <br />
+                <label>
+                    Password:
+                    <input type="text" {...register('password')} />
+                </label>
+                { formState.errors.password && formState.touchedFields.password && (
+                    <p>{formState.errors.password.message}</p>
                 )}
                 <br />
                 <button type="submit">Submit</button>
             </form>
             <div>
-                <p>First name: {values.firstName}</p>
-                <p>Last name: {values.lastName}</p>
+                <p>Email: {values.email}</p>
+                <p>User Name: {values.userName}</p>
+                <p>Password: {values.password}</p>
             </div>
         </>
     )
