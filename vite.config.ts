2177b8daa38d5239ba3267cfe1aeec7a6504baa3
ShlykovAA <anatolio7z@gmail.com>
2023-06-14 19:23:24 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import checker from "vite-plugin-checker";
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    global: {},
+    global: "window",
   },
   plugins: [
     react(),
